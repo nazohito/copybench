cdf682e3e83dac6e3470ff4beec2c09eaf9046f9
kazuki.ohta <kazuki.ohta@9f08360b-7e4c-0410-bc79-45d700458da5>
2008-05-05 18:28:19 +0000

    * test.sh   - fix to use bash

--- trunk/test.sh
+++ trunk/test.sh
@@ -7,14 +7,14 @@ PREPARE='rm $DST; sync; sync; sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_cache
 REPEAT=1
 
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./rw_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./rw_cp ${SRC} ${DST}; done;)
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./rw_fadv_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./rw_fadv_cp ${SRC} ${DST}; done;)
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./mm_sync_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./mm_sync_cp ${SRC} ${DST}; done;)
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./mm_sync_madv_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./mm_sync_madv_cp ${SRC} ${DST}; done;)
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./mw_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./mw_cp ${SRC} ${DST}; done;)
 eval $PREPARE
-(repeat $REPEAT $TIMEX ./mw_madv_cp ${SRC} ${DST})
+(for i in `seq 0 $REPEAT`; do $TIMEX ./mw_madv_cp ${SRC} ${DST}; done;)
