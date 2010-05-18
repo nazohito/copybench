6db2567e1c6f1c44b8e5197aa15fdb10ba8fa30e
kazuki.ohta <kazuki.ohta@9f08360b-7e4c-0410-bc79-45d700458da5>
2010-05-18 15:29:08 +0000

    merge test.sh from kosaki.motohiro. fixes Issue 1.

--- trunk/test.sh
+++ trunk/test.sh
@@ -1,14 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
 
-SRC=/data/5Gfile
-DST=/data2/5Gfile
-TIMEX=time
-PREPARE='rm $DST; sync; sync; sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"'
-REPEAT=1
+DIR=`pwd`
 
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./rw_cp ${SRC} ${DST}; done;)
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./rw_fadv_cp ${SRC} ${DST}; done;)
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./mm_sync_cp ${SRC} ${DST}; done;)
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./mm_sync_madv_cp ${SRC} ${DST}; done;)
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./mw_cp ${SRC} ${DST}; done;)
-(for i in `seq 1 $REPEAT`; do eval $PREPARE; $TIMEX ./mw_madv_cp ${SRC} ${DST}; done;)
+SRC=$DIR/srcfile
+DST=$DIR/dstfile
+TIMEX="/usr/bin/time -p"
+PREPARE='rm $DST >& /dev/null ; sync; sync; sync; sudo sh -c "echo 3 > 
+/proc/sys/vm/drop_caches"'
+REPEAT=3
+
+if [ ! -f $SRC ]; then
+    echo "[ERROR] Please create src file: path=$SRC" 1>&2
+    exit 1
+fi
+
+for test in rw_cp rw_fadv_cp mm_sync_cp mm_sync_madv_cp mw_cp mw_madv_cp; do
+  echo $test
+  for i in `seq 1 $REPEAT`; do
+      eval $PREPARE;
+      $TIMEX ./${test} ${SRC} ${DST} 2>&1 | grep real;
+  done
+  echo
+done
